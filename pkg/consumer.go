10d5c9f21c327df368115874bd9a0f7f6544ca90
Clement Ng <clement.ng@dynata.com>
2021-02-14 12:10:26 -0800

    add some configs to play with

--- pkg/consumer.go
+++ pkg/consumer.go
@@ -2,6 +2,7 @@ package kappa
 
 import (
 	"context"
+	"time"
 
 	"github.com/segmentio/kafka-go"
 	"github.com/sirupsen/logrus"
@@ -11,11 +12,13 @@ type consumer interface {
 	consume(ctx context.Context)
 }
 
+// Consumer ...
 type Consumer struct {
 	ReaderConfig kafka.ReaderConfig
 	Logger       *logrus.Entry
 }
 
+// Consume ...
 func (c *Consumer) Consume(ctx context.Context) {
 
 	kr := kafka.NewReader(c.ReaderConfig)
@@ -26,15 +29,22 @@ func (c *Consumer) Consume(ctx context.Context) {
 			panic("failed to read message " + err.Error())
 		}
 
-		c.Logger.WithField("message: ", string(msg.Value)).Info("Received message")
+		c.Logger.WithField("message: ", string(msg.Value)).WithField("time read: ", time.Now()).Info("Received message")
 	}
 }
 
 // NewReaderConfig ...
 func NewReaderConfig(topic string, brokers []string) kafka.ReaderConfig {
+	// logger := log.New(os.Stdout, "kafka consumer: ", 0)
 	return kafka.ReaderConfig{
-		Brokers: brokers,
-		Topic:   topic,
-		GroupID: "kafka-test-group",
+		// Logger:  logger,
+		Brokers:     brokers,
+		Topic:       topic,
+		GroupID:     "kafka-test-group",
+		MinBytes:    5,                 // min # of bytes to be read
+		MaxBytes:    1e4,               // max # of bytes to be read
+		MaxWait:     3 * time.Second,   // wait at most 3 secs before polling for new data
+		StartOffset: kafka.FirstOffset, // consume earliest messages available
+		// StartOffset: kafka.LastOffset, // consume the newest messages available after startup
 	}
 }
--- pkg/producer.go
+++ pkg/producer.go
@@ -13,11 +13,13 @@ type producer interface {
 	produce(ctx context.Context)
 }
 
+// Producer ...
 type Producer struct {
 	WriterConfig kafka.WriterConfig
 	Logger       *logrus.Entry
 }
 
+// Produce ...
 func (p *Producer) Produce(ctx context.Context) {
 	i := 0
 
@@ -32,18 +34,29 @@ func (p *Producer) Produce(ctx context.Context) {
 			p.Logger.WithField("err", err).Error("Failed to write message")
 			panic("failed to write message id " + strconv.Itoa(i) + " " + err.Error())
 		}
-		p.Logger.WithField("Message ID: ", i).Info("Successfully wrote message")
+		p.Logger.WithField("Message ID: ", i).WithField("time wrote: ", time.Now()).Info("Successfully wrote message")
 
 		i++
-		time.Sleep(time.Second)
+		//time.Sleep(500 * time.Millisecond)
 
 	}
 }
 
 // NewWriterConfig ...
 func NewWriterConfig(topic string, brokers []string) kafka.WriterConfig {
+	// logger := log.New(os.Stdout, "kafka producer: ", 0)
 	return kafka.WriterConfig{
-		Brokers: brokers,
-		Topic:   topic,
+		// Logger:       logger,
+		Brokers:      brokers,
+		Topic:        topic,
+		BatchSize:    5,               // wait until 5 messages before writing
+		BatchTimeout: 5 * time.Second, // after 4 seconds, write all pending messages
+		RequiredAcks: -1,
 	}
+	/*
+		Required Acks -- Required Acknowledgements
+		-1 = All brokers received the message
+		0 =  No brokers acknowledge receiving the message
+		1 = Leader broker acknowledges receiving the message
+	*/
 }
